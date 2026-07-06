11bc581fc31ea22304a5518bdd4e51d8b673ba88
SlyGuy <juanriis456@gmail.com>
2026-07-05 20:15:56 -0500

    Updated the README.md to state some information about the project. Added some notes in the globals.css. Updated the package.json to include the react-icons package removed some of the boilerplate from layout, and page. Started the SearchBar Creation to make it work and pull some data.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# trade-supplies
+# trade supplies
+
+Trade supplies is the worlds easiest to find construction supplies and tool locator app. 
+
+Here you can search and find all types of materials for all kinds of trades.
---
+++ frontend/components/searchBar/searchBar.tsx
@@ -0,0 +1,13 @@
+
+
+export function SearchBar(){
+  return(
+    <>
+      <input
+        type="text"
+        placeholder="Search for construction tools and supplies"
+        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+      />
+    </>
+  )
+}
