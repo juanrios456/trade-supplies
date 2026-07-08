4c02c9aa648ff6cf55495d79c62debd51789f7aa
SlyGuy <juanriis456@gmail.com>
2026-07-08 13:13:29 -0500

    Added asp.net api as a backend. added to the root folder gitignore to hide files with sensetive data and ignore files that are unnecesary. Chaged the frontend landing page to show test connection

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,37 @@
-import SearchBar from "@components/searchBar/searchBar";
+import {SearchBar} from "@components/searchBar/searchBar";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  //Core Endpoint
+  const response = await fetch(`${apiUrl}/api/weatherforecast`, {
+    cache: "no-store",
+  });
+
+  const data = await response.json();
+
   return (
     <div>
       <h1>Welcome to Trade Supplies</h1>
       <p>Search Below to find construction tools supplies for all types of construction trades.</p>
       <p>(Use the filter category to order your search based on your priorities.)</p>
       <SearchBar />
+      <div>
+        <h1>ASP.NET TEST JSON</h1>
+        <p>{JSON.stringify(data, null, 2)}</p>
+        <ul>
+          {data.map((item: any) => (
+            <li key={item.date}>
+              <p>Date: {item.date}</p>
+              <p>TemperatureC: {item.temperatureC}</p>
+              <p>TemperatureF: {item.temperatureF}</p>
+              <p>Summary: {item.summary}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
